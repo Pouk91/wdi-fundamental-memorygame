d031961670bf947e93e95a5032e524da2f227da1
Pinaro Ouk <Pinaro Ouk>
2017-03-22 21:17:55 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "King";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+var cardOne = cards[0];
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardTwo = cards[2];
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === "2") {
+  if (cardsInPlay[0] === cardsInPlay[2]) {}
+alert("You found a match");
+}  else {
+alert("Sorry,try again.");
+}
